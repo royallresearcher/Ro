4db3d50f72a3c3d6cc894c805b68f34fd6bd38cf
Royall <105764918+royallresearchers@users.noreply.github.com>
2023-11-19 08:06:50 +0530

    Add files via upload

---
+++ subtraction_1(normal).py
@@ -0,0 +1,11 @@
+def subtract_integers():
+    num1 = int(input("Enter the first integer: "))
+    num2 = int(input("Enter the second integer: "))
+    result = num1 - num2
+    print(f"The result of {num1} - {num2} is: {result}")
+
+# Uncomment the line below to run this method
+# subtract_integers()
+
+if __name__ == "__main__":
+    subtract_integers()
